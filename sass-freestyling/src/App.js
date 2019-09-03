67f2fe60f69dee5224bdcb2f7db5f99e85233b11
Archbold <archiboldpatsanza@gmail.com>
2019-09-03 13:20:05 +0300

    adding multiple children

--- sass-freestyling/src/App.js
+++ sass-freestyling/src/App.js
@@ -13,11 +13,14 @@ class App extends React.Component{
       mother:'',
       child:''
     }
-    this.appender=this.appender.bind(this)
   }
 
 appender=(e)=>{
       e.preventDefault();
+      let d=document.createElement('div');
+      d.innerHTML=`<div class="family"><div class="row"><div class="father">${this.state.father}</div><div class="mother">${this.state.mother}</div></div><div class="row"><div class="child">${this.state.child}</div></div></div>`;
+      document.querySelector('#container').appendChild(d);
+      //console.log(d);
   }
 handleChange=(e)=>{
     e.target.getAttribute('relation')==='father' ? this.setState({father:e.target.value}):e.target.getAttribute('relation')==='mother' ? this.setState({mother:e.target.value}):this.setState({child:e.target.value});
